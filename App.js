08286f9659c0f9efe030e57992bb2e8621a6dc07
sujaany <ssujaany09@gmail.com>
2019-07-05 16:24:51 +0530

    finished react native practical

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import LockScreen from "./components/lockScreen";
+
+export default class App extends Component {
+  render() {
+    return <LockScreen />;
+  }
+}
